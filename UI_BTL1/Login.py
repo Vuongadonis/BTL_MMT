263435862f9fe4cad1adb1a047954896625697da
vuong <vuong03062003@gmail.com>
2023-12-03 22:48:34 +0700

    forget this commit

--- UI_BTL1/Login.py
+++ UI_BTL1/Login.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import subprocess
 
-list_account = ["giaqui","hoangvuong","trungkien","anhkhoa"]
+list_account = ["giaqui", "hoangvuong", "trungkien", "anhkhoa"]
 
 root = Tk()
 root.title("Login")
@@ -12,15 +12,16 @@ root.resizable(False, False)
 
 
 def sign_in():
-    if(user.get() in list_account and password.get() == "12345"):
+    if (user.get() in list_account and password.get() == "12345"):
         root.destroy()
         subprocess.call(['python', 'peerUI.py'])
     else:
-        messagebox.showerror("Lỗi","Tài khoản hoặc mặt khẩu không đúng!"
-                                   "\n\nVui lòng kiểm tra lại.")
+        messagebox.showerror("Lỗi", "Tài khoản hoặc mặt khẩu không đúng!"
+                             "\n\nVui lòng kiểm tra lại.")
+
 
 img = PhotoImage(file='img.png')
-Label(root,image=img,bg='white').place(x=50,y=50)
+Label(root, image=img, bg='white').place(x=50, y=50)
 
 frame = Frame(root, width=350, height=350, bg='white')
 frame.place(x=480, y=70)
@@ -30,35 +31,46 @@ heading = Label(frame, text='Đăng Nhập', fg='#57a1f8', bg='white',
                 font=('Microsolf Yahei UI Light', 23, 'bold'))
 heading.place(x=100, y=5)
 # ===================== Tên đăng nhập ========================
+
+
 def on_enter(e):
-    user.delete(0,'end')
+    user.delete(0, 'end')
+
+
 def on_leave(e):
     name = user.get()
-    if(name == ''):
-        user.insert(0,'Tên đăng nhập')
+    if (name == ''):
+        user.insert(0, 'Tên đăng nhập')
+
 
 user = Entry(frame, width=25, fg='black', border=0,
              bg='white', font=('Microsolf Yahei UI Light', 11))
 user.place(x=30, y=80)
 user.insert(0, 'Tên đăng nhập')
-user.bind('<FocusIn>',on_enter)
-user.bind('<FocusOut>',on_leave)
+user.bind('<FocusIn>', on_enter)
+user.bind('<FocusOut>', on_leave)
 
 Frame(frame, width=295, height=2, bg='black',).place(x=25, y=107)
 
 # ==================== Mật  khẩu ================================
+
+
 def on_enter(e):
-    password.delete(0,'end')
+    password.delete(0, 'end')
+
+
 def on_leave(e):
     name = password.get()
-    if(name == ''):
-        password.insert(0,'Mật khẩu')
+    if (name == ''):
+        password.insert(0, 'Mật khẩu')
+
+
 password = Entry(frame, width=25, fg='black', border=0,
                  bg='white', font=('Microsolf Yahei UI Light', 11))
 password.place(x=30, y=150)
 password.insert(0, 'Mật khẩu')
-password.bind('<FocusIn>',on_enter)
-password.bind('<FocusOut>',on_leave)
+password.bind('<FocusIn>', on_enter)
+password.bind('<FocusOut>', on_leave)
 
 Frame(frame, width=295, height=2, bg='black',).place(x=25, y=177)
 
